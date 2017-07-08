6e8a03a9bd25ad56f03ab8bc77d5912c5d90329d
jonathan <aguilaj10@gmail.com>
2017-07-07 23:48:41 -0500

    - Agregué pantalla de inicio de sesión - Agregué iconos y selectors para la pantalla de inicio de sesión - Agregué una dependencia para EditText - Agregué estilos

--- app/src/main/java/com/mbn/movil/view/impl/MainActivity.java
+++ app/src/main/java/com/mbn/movil/view/impl/MainActivity.java
@@ -2,6 +2,7 @@ package com.mbn.movil.view.impl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.mbn.movil.R;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
