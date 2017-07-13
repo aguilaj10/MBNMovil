90456e42f0a5a6b51a563fc52d1de71512751728
Annel Gonzalez <annelkaren@gmail.com>
2017-07-13 14:02:44 -0500

    Agregar nueva actividad para cambio de contrasena

--- app/src/main/java/com/mbn/movil/view/impl/ActualizarContrasenaActivity.java
+++ app/src/main/java/com/mbn/movil/view/impl/ActualizarContrasenaActivity.java
@@ -7,14 +7,14 @@ import android.util.Log;
 
 import com.mbn.movil.R;
 
-public class CambiarContrasena extends AppCompatActivity {
+public class ActualizarContrasenaActivity extends AppCompatActivity {
 
-    public static final String TAG = CambiarContrasena.class.getSimpleName();
+    public static final String TAG = ActualizarContrasenaActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cambiar_contrasena);
+        setContentView(R.layout.activity_actualizar_contrasena);
         Intent data = getIntent();
         String action = data.getData().getPath();
 
