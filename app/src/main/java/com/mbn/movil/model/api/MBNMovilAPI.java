fa27e4d8415726ccb88a5adc48b8ea170e68b2d5
Annel Gonzalez <annelkaren@gmail.com>
2017-07-13 21:01:42 -0500

    Cambios para cambio de contraseña, la funcionalidad para envio de correo está completa

--- app/src/main/java/com/mbn/movil/model/api/MBNMovilAPI.java
+++ app/src/main/java/com/mbn/movil/model/api/MBNMovilAPI.java
@@ -5,7 +5,10 @@ import com.mbn.movil.model.dto.UsuarioDTO;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by jonathan on 11/07/17.
@@ -15,4 +18,7 @@ public interface MBNMovilAPI {
 
     @POST ("mbn-service/ws/iniciarSesion/")
     Call<UsuarioDTO> iniciarSesion(@Body UsuarioDTO dto);
+
+    @GET("cambiarContrasena")
+    Call<UsuarioDTO> cambiarContrasena(@Query("correo") String correo);
 }
